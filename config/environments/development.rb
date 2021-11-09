12ed60a9c7a2f742d1d9de047bb2921f72c0bf21
Fabio Miranda <fabio@miranti.net.br>
2021-11-09 20:42:51 -0300

    Resolves warning 'Cannot render console from 172.26.0.1!'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.web_console.permissions = '172.0.0.0/8'
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
